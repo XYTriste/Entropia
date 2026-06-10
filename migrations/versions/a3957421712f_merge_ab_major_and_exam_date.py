1c69890d1e8037b4cc5ca7e4a5adcf5b2274623a
XYTriste <1106693417@qq.com>
2026-06-10 16:25:22 +0800

    chore(migration): merge ab_major_preference and exam_date migrations

---
+++ migrations/versions/a3957421712f_merge_ab_major_and_exam_date.py
@@ -0,0 +1,25 @@
+"""merge_ab_major_and_exam_date
+
+Revision ID: a3957421712f
+Revises: 6884e3d3c637, 9b84789eae36
+Create Date: 2026-06-10 08:24:52.333517
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a3957421712f'
+down_revision = ('6884e3d3c637', '9b84789eae36')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
